20b1f4eba63be9858f0cfee144a71473fb6e6db0
quangdeeptry <quangdeeptry7@gmail.com>
2021-04-29 17:36:27 +0700

    quangupdate lai ngay 29/4

--- nodejs/controllers/queries.js
+++ nodejs/controllers/queries.js
@@ -11,3 +11,17 @@ const getPosts = modelPosts.find(objectData).populate({
 BlogPost.countDocuments(conditionFind);
 // lam tron so le 1.8 => 2, 1.4 => 1
 Match.ceil();
+
+// lookup trong query mongoose
+// from la ten bang, localField la ten truong cua mot bang khac
+// foreignField la mot truong trong tenBang 
+// as la la mot cai ten gi do minh thich dat sao cung, kieu nhu 1 bien dau` ra
+lookup({
+  from: 'tenBang', localField: '_id', foreignField: 'categories', as: 'posts',
+})
+// project trong query mongoose
+// name va createAt la 2 thuoc tinh trong Post
+// gia tri name: 1 => 1 or true la van giu quyen gia tri cua ten bang khi goi ra
+//  totalPosts va totalChildren lay thuoc tinh cua as tren lookup
+Post.find().project({ name: 1,createdAt: 1,totalPosts: { $size: '$posts' },totalChildren: { $size: '$children' },
+});
